800e3209635bb9d368b9dac537389759fd07ebd4
Pavel Protopopov <p0chta3@yandex.ru>
2025-11-18 20:38:02 +0800

    Update.

--- opinions_app/forms.py
+++ opinions_app/forms.py
@@ -1,4 +1,5 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileAllowed, MultipleFileField
 from wtforms import StringField, SubmitField, TextAreaField, URLField
 from wtforms.validators import DataRequired, Length, Optional
 
@@ -10,11 +11,22 @@ class OpinionForm(FlaskForm):
                     Length(1, 128)]
     )
     text = TextAreaField(
-        'Напишите мнение', 
+        'Напишите мнение',
         validators=[DataRequired(message='Обязательное поле')]
     )
     source = URLField(
         'Добавьте ссылку на подробный обзор фильма',
         validators=[Length(1, 256), Optional()]
+    )
+    images = MultipleFileField(
+        validators=[
+            FileAllowed(
+                ['jpg', 'jpeg', 'png', 'gif', 'bmp'],
+                message=(
+                    'Выберите файлы с расширением '
+                    '.jpg, .jpeg, .png, .gif или .bmp'
+                )
+            )
+        ]
     )
     submit = SubmitField('Добавить')
--- opinions_app/models.py
+++ opinions_app/models.py
@@ -8,6 +8,7 @@ class Opinion(db.Model):
     title = db.Column(db.String(128), nullable=False)
     text = db.Column(db.Text, unique=True, nullable=False)
     source = db.Column(db.String(256))
+    images = db.Column(db.JSON)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     added_by = db.Column(db.String(64))
 
@@ -25,4 +26,3 @@ class Opinion(db.Model):
         for field in ['title', 'text', 'source', 'added_by']:
             if field in data:
                 setattr(self, field, data[field])
-                
